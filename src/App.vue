4c4bcc44c215de32f7f6987199a27cdd6cab4ec9
Serom <serom@Seromui-MacBook-Pro.local>
2019-04-21 22:56:38 +0900

    accordion components

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <accordion title="hello">
+      <p>Lorem ipsum dolor sit amet, ut alii voluptaria est, ad illum inimicus deterruisset eam. His eu bonorum adipisci definiebas, no vis nostrud conclusionemque. Ad his virtute accusata, pro habemus singulis temporibus ut, ne bonorum dolores euripidis quo. No nam amet erant intellegebat. Rationibus instructior id pri, vis case abhorreant ea, id sea meis feugiat.</p>
+    </accordion>
+    <accordion title="example1">
+      <p>Lorem ipsum dolor sit amet, ut alii voluptaria est, ad illum inimicus deterruisset eam. His eu bonorum adipisci definiebas, no vis nostrud conclusionemque. Ad his virtute accusata, pro habemus singulis temporibus ut, ne bonorum dolores euripidis quo. No nam amet erant intellegebat. Rationibus instructior id pri, vis case abhorreant ea, id sea meis feugiat.</p>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, ut alii voluptaria est, ad illum inimicus deterruisset eam. His eu bonorum adipisci definiebas, no vis nostrud conclusionemque. Ad his virtute accusata, pro habemus singulis temporibus ut, ne bonorum dolores euripidis quo. No nam amet erant intellegebat. Rationibus instructior id pri, vis case abhorreant ea, id sea meis feugiat.</p>
+    </accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/accordion'
+
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './sass/App';
+  @font-face {
+    font-family: 'Nanum Square';
+    src: url('./assets/font/NanumSquareR.eot');
+    src: url('./assets/font/NanumSquareR.eot?#iefix') format('embedded-opentype'), url('./assets/font/NanumSquareR.woff') format('woff'), url('./assets/font/NanumSquareR.ttf') format('truetype'), url('./assets/font/NanumSquareOTFR.svg#9e23ceb0c858ca37642ba540402577eb') format('svg');
+    font-style: normal;
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'Nanum Square';
+    src: url('./assets/font/NanumSquareB.eot');
+    src: url('./assets/font/NanumSquareB.eot?#iefix') format('embedded-opentype'), url('./assets/font/NanumSquareB.woff') format('woff'), url('./assets/font/NanumSquareB.ttf') format('truetype'), url('./assets/font/NanumSquareOTFB.svg#a41a1fda1fdf3dafd3394867a156b1cf') format('svg');
+    font-style: normal;
+    font-weight: 700;
+  }
+
+  #app {
+    font-family: 'Nanum Square', 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: left;
+    color: #000;
+  }
+</style>
---
+++ src/components/accordion.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="cover">
+    <h1 class="accordion-title">
+      <span class="down-Arrow" v-show="!active">&#9660;</span>
+      <span class="up-Arrow" v-show="active">&#9650;</span>
+      <button type="button" @click.prevent="toggle">{{title}}</button>
+    </h1>
+    <transition
+      @enter="startTransition"
+      @after-enter="startTransition"
+      @before-leave="startTransition"
+      @after-leave="endTransition"
+    >
+      <div class="content" v-show="active">
+        <slot />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'accordion',
+  props: [
+    'title'
+  ],
+  data () {
+    return {
+      active: false
+    }
+  },
+
+  methods: {
+    toggle (event) {
+      this.active = !this.active
+    },
+
+    startTransition (el) {
+      console.log('startTransition :::', el.scrollHeight + 'px')
+      el.style.height = el.scrollHeight + 'px'
+    },
+
+    endTransition (el) {
+      console.log('endTransition :::', el.scrollHeight + 'px')
+      el.style.height = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.cover{
+  position: relative;
+  width: 500px;
+  margin: 0 auto;
+  border-radius: 4px;
+  background: lightcyan;
+  h1.accordion-title{
+    width: 100%;
+    border-bottom: 1px solid #dedfdf;
+    padding: 20px;
+    box-sizing: border-box;
+    span {
+      display: inline-block;
+    }
+    button{
+      display: inline-block;
+      background: none;
+      border: 0 none;
+    }
+  }
+  .content{
+    background: lightgray;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+}
+
+.accordion-leave,
+.accordion-enter-to{
+  height: auto;
+}
+
+.accordion-enter-active,
+.accordion-leave-active {
+  transition: all 5s ease;
+}
+.accordion-enter,
+.accordion-leave-to {
+  height: 0;
+}
+
+</style>
